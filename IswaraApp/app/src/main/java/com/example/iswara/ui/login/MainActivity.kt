f7e170551defa693d839960d78b63eae2fa42016
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-05-19 12:59:57 +0700

    delete fragment-ktx inside main activity

--- IswaraApp/app/src/main/java/com/example/iswara/ui/login/MainActivity.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/login/MainActivity.kt
@@ -11,16 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /* buat cek nampilin fragment di main activity */
-        val mFragmentManager = supportFragmentManager
-        val mHomeFragment = BerandaFragment()
-        val fragment = mFragmentManager.findFragmentByTag(BerandaFragment::class.java.simpleName)
-
-        if (fragment !is BerandaFragment) {
-            mFragmentManager.commit {
-                add(R.id.frame_container, mHomeFragment, BerandaFragment::class.java.simpleName)
-            }
-        }
     }
 }
