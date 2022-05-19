a2a390c3e05c26f78ea1225e2a85c474a7cf479d
widigdacahya <rizkiwidigda@gmail.com>
2022-05-19 09:32:13 +0700

    add toast btn beranda
    toast for konsultasi,ruang cerita, darurat, and floatingactionbar ara

--- IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaFragment.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.iswara.R
@@ -43,6 +44,22 @@ class BerandaFragment : Fragment() {
         //removing bg and shadow things of bottom nav menu
         berandaBinding?.bottomNavViewMenu?.background = null
         berandaBinding?.bottomNavViewMenu?.menu?.getItem(1)?.isEnabled =false
+
+        berandaBinding?.cvRuangan?.setOnClickListener {
+            Toast.makeText(activity,"Ruangan will available soon \uD83E\uDD70", Toast.LENGTH_SHORT).show()
+        }
+
+        berandaBinding?.cvKonsultasi?.setOnClickListener {
+            Toast.makeText(activity,"Thanks for click this.Konsultasi will available soon \uD83D\uDE4F", Toast.LENGTH_SHORT).show()
+        }
+
+        berandaBinding?.cvDarurat?.setOnClickListener {
+            Toast.makeText(activity,"\uD83D\uDE4B\uD83C\uDFFB\u200D♀️Excuse me, Darurat Feature will available soon", Toast.LENGTH_SHORT).show()
+        }
+
+        berandaBinding?.fabChatbotAra?.setOnClickListener {
+            Toast.makeText(activity,"Opening chat bot Ara ❤️ ", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroy() {
