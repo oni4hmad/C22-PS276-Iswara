b18ef73df9393bedde62e3c217ba285761658cc6
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-05-21 13:47:15 +0700

    add list cerita layout
    including its data class, recycler view adapter, card layout, fragment, view model (but still using faker for generating data)

---
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/CeritaItem.kt
@@ -0,0 +1,15 @@
+package com.example.iswara.ui.ruang_cerita
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.*
+
+@Parcelize
+data class CeritaItem(
+    val idCerita: String,
+    val name: String,
+    val date: Date,
+    val cerita: String,
+    val tanggapanCount: Int,
+    val supportCount: Int
+) : Parcelable
---
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/ListCeritaAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.iswara.ui.ruang_cerita
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.iswara.databinding.ItemRowCeritaBinding
+import java.text.SimpleDateFormat
+
+class ListCeritaAdapter(private val listCerita: List<CeritaItem>) : RecyclerView.Adapter<ListCeritaAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemRowCeritaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listCerita[position])
+    }
+
+    override fun getItemCount(): Int = listCerita.size
+
+    inner class ListViewHolder(private var binding: ItemRowCeritaBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(cerita: CeritaItem) {
+
+//            binding.ivUser.setBackgroundColor(Color.rgb(255, 0, 0))
+//            binding.ivUser.setBackgroundResource(R.color.orange)
+            Glide.with(binding.ivUser.context)
+                .load("https://loremflickr.com/200/100")
+                .into(binding.ivUser)
+
+            binding.tvName.text = cerita.name
+            binding.tvDate.text = cerita.date.let {
+                val formatter = SimpleDateFormat("d MMMM yyyy")
+                formatter.format(it)
+            }
+            binding.tvCerita.text = cerita.cerita.let {
+                if (it.length > 300) {
+                    binding.tvReadMore.visibility = View.VISIBLE
+                    it.take(300) + "..."
+                } else {
+                    binding.tvReadMore.visibility = View.GONE
+                    it
+                }
+            }
+            binding.tvTanggapan.text = "${cerita.tanggapanCount} tanggapan"
+            binding.tvSupport.text = "${cerita.supportCount} support"
+
+            binding.tvReadMore.setOnClickListener { onItemClickCallback.onItemClicked(cerita) }
+            binding.cvItem.setOnClickListener { onItemClickCallback.onItemClicked(cerita) }
+        }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(cerita: CeritaItem)
+    }
+}
