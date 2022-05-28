a55c5a9dc45d279876947a6723895986f5f8b355
melftr <95953709+melftr@users.noreply.github.com>
2022-05-28 19:42:51 +0700

    merge test file

--- chatbot/SimpleChatbot/test.py
+++ chatbot/SimpleChatbot/test.py
@@ -5,7 +5,6 @@ import random
 import pickle
 from tensorflow.keras.models import load_model
 
-with open("src/humanText.json") as data_file:
     data = json.load(data_file)
 model = load_model('modelLSTM.tf')
 le_filename = open("label_encoder.pickle", "rb")
