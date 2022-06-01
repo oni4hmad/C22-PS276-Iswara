31ffd5ad626b94a86e1801c404e0915f3d2356b4
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-06-02 03:57:29 +0700

    add fab ruang cerita

--- IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/cerita_tablayout/TabCeritaFragment.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/cerita_tablayout/TabCeritaFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.viewpager2.widget.ViewPager2
 import com.example.iswara.R
@@ -31,6 +32,7 @@ class TabCeritaFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(TabCeritaViewModel::class.java)
 
+        /* tab layout: section pager adapter, view pager */
         val sectionsPagerAdapter = SectionsPagerAdapter(requireActivity() as MainActivity)
         val viewPager: ViewPager2 = binding.vpCerita
         viewPager.adapter = sectionsPagerAdapter
@@ -39,6 +41,15 @@ class TabCeritaFragment : Fragment() {
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
         (requireActivity() as MainActivity).supportActionBar?.elevation = 0f
+
+        /* fab: add cerita */
+        binding.fabAddCerita.setOnClickListener {
+            showToast("add cerita!")
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(view?.context, text, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
