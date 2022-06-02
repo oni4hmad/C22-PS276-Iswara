e928da80f5530cc87900d1620cc80b49f4a66a1a
widigdacahya <rizkiwidigda@gmail.com>
2022-06-03 00:04:01 +0700

    register fragment not shown in nav graph

--- IswaraApp/app/src/main/java/com/example/iswara/ui/register/RegisterFragment.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/register/RegisterFragment.kt
@@ -17,7 +17,7 @@ class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
