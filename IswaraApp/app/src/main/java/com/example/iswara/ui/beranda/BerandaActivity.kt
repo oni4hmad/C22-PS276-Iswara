e442071ac8bb6b8bd69ae31e02651da0da5e7ecf
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-06-04 20:15:31 +0700

    fix beranda btm navigation bar

--- IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaActivity.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaActivity.kt
@@ -1,48 +1,30 @@
 package com.example.iswara.ui.beranda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
-import androidx.core.view.get
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.iswara.R
 import com.example.iswara.databinding.ActivityBerandaBinding
-import com.example.iswara.ui.settings.SettingsFragment
 
 class BerandaActivity : AppCompatActivity() {
 
-    private lateinit var berandaActivityBinding: ActivityBerandaBinding
+    private lateinit var binding: ActivityBerandaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        berandaActivityBinding = ActivityBerandaBinding.inflate(layoutInflater)
-
-        setContentView(berandaActivityBinding.root)
-
-
-        berandaActivityBinding.bottomNavViewMenu.background = null
-        berandaActivityBinding.bottomNavViewMenu.menu.getItem(1).isEnabled = false
-
-
-
-
+        binding = ActivityBerandaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val theNavController = findNavController(R.id.fragmentContainerView2)
-        berandaActivityBinding.bottomNavViewMenu.setupWithNavController(theNavController)
+        binding.bottomNavViewMenu.setupWithNavController(theNavController)
 
-        berandaActivityBinding.fabChatbotAra.setOnClickListener {
+        binding.fabChatbotAra.setOnClickListener {
             Toast.makeText(this,"Chatbot here", Toast.LENGTH_SHORT).show()
             theNavController.navigate(R.id.action_berandaFragment2_to_chatbotActivity)
-
         }
 
-
     }
-
-
 
 }
--- IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaFragment.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/beranda/BerandaFragment.kt
@@ -15,50 +15,41 @@ import com.example.iswara.databinding.FragmentBerandaBinding
 
 class BerandaFragment : Fragment() {
 
-    private var _berandaBinding : FragmentBerandaBinding? = null
-    private val berandaBinding get() = _berandaBinding
+    private lateinit var binding : FragmentBerandaBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+        binding = FragmentBerandaBinding.inflate(inflater,  container, false)
+        return binding.root
     }
 
     @SuppressLint("RestrictedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        _berandaBinding = FragmentBerandaBinding.bind(view)
-
         (activity as AppCompatActivity?)?.supportActionBar?.apply {
             setShowHideAnimationEnabled(false)
             hide()
         }
 
-        berandaBinding?.cvRuangan?.setOnClickListener {
+        binding.cvRuangan.setOnClickListener {
             Toast.makeText(activity,"Ruangan will available soon \uD83E\uDD70", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_berandaFragment2_to_tabCeritaActivity)
         }
 
-        berandaBinding?.cvKonsultasi?.setOnClickListener {
+        binding.cvKonsultasi.setOnClickListener {
             Toast.makeText(activity,"Thanks for click this.Konsultasi will available soon \uD83D\uDE4F", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_berandaFragment2_to_konsultasiSoonFragment)
         }
 
-        berandaBinding?.cvDarurat?.setOnClickListener {
+        binding.cvDarurat.setOnClickListener {
             Toast.makeText(activity,"\uD83D\uDE4B\uD83C\uDFFB\u200D♀️Excuse me, Darurat Feature will available soon", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_berandaFragment2_to_daruratFragment)
         }
 
 
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _berandaBinding = null
     }
 
 }
