9f3b5771caa3fe2fecea0d6856346ee97ed0497e
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-06-13 07:05:25 +0700

    add cerita mockapi

--- IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/add_cerita/AddCeritaFragment.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/add_cerita/AddCeritaFragment.kt
@@ -1,21 +1,28 @@
 package com.example.iswara.ui.ruang_cerita.add_cerita
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.iswara.R
+import com.example.iswara.data.preferences.Session
+import com.example.iswara.data.preferences.SessionPreference
 import com.example.iswara.databinding.FragmentAddCeritaBinding
-import com.example.iswara.ui.ruang_cerita.cerita_tablayout.TabCeritaActivity
-import com.example.iswara.ui.ruang_cerita.detail_tanggapan.DetailTanggapanViewModel
+import com.example.iswara.utils.dateToString
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddCeritaFragment : Fragment() {
 
     private lateinit var binding: FragmentAddCeritaBinding
     private lateinit var viewModel: AddCeritaViewModel
+    private lateinit var session: Session
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,12 +35,28 @@ class AddCeritaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(this).get(AddCeritaViewModel::class.java)
+        SessionPreference(view.context).getSession()?.also {
+            session = it
+        }
+
+        viewModel = ViewModelProvider(this)[AddCeritaViewModel::class.java]
+        viewModel.isSucceed.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let { isSucceed ->
+                if (isSucceed) toStoryList("Story added successfully")
+            }
+        }
+        viewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+        viewModel.toastText.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let { toastText ->
+                showToast(toastText)
+            }
+        }
 
         Glide.with(binding.ivUser3.context)
             .load("https://loremflickr.com/200/100")
             .into(binding.ivUser3)
-
 
         (activity as AppCompatActivity).supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
@@ -44,13 +67,38 @@ class AddCeritaFragment : Fragment() {
 
     }
 
+    private fun toStoryList(msg: String) {
+        val toListStoryFragment = AddCeritaFragmentDirections.actionAddCeritaFragment2ToTabCeritaFragment2(msg)
+        view?.findNavController()?.navigate(toListStoryFragment)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            (activity as AppCompatActivity).supportActionBar?.hide()
+            binding.layoutBgLoading.visibility = View.VISIBLE
+        } else {
+            (activity as AppCompatActivity).supportActionBar?.show()
+            binding.layoutBgLoading.visibility = View.GONE
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_cerita_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
-        menu.clear()
+//        menu.clear()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
+            R.id.add_story -> {
+                val ceritaToPost = binding.edtCerita.text.toString()
+                if (ceritaToPost.isNotEmpty()) {
+                    viewModel.postStory(session, dateToString(Date()), ceritaToPost)
+                } else {
+                    binding.edtCerita.error = "Couldn't be empty"
+                }
+                return true
+            }
             android.R.id.home -> {
                 findNavController().navigateUp()
                 return true
@@ -59,6 +107,8 @@ class AddCeritaFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-
+    private fun showToast(text: String) {
+        Toast.makeText(view?.context, text, Toast.LENGTH_SHORT).show()
+    }
 
 }
--- IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/add_cerita/AddCeritaViewModel.kt
+++ IswaraApp/app/src/main/java/com/example/iswara/ui/ruang_cerita/add_cerita/AddCeritaViewModel.kt
@@ -1,7 +1,99 @@
 package com.example.iswara.ui.ruang_cerita.add_cerita
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.iswara.data.network.ApiConfig
+import com.example.iswara.data.network.Cerita
+import com.example.iswara.data.preferences.Session
+import com.example.iswara.ui.Event
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class AddCeritaViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AddCeritaViewModel() : ViewModel() {
+
+    companion object {
+        private const val TAG = "AddCeritaViewModel"
+    }
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _toastText = MutableLiveData<Event<String>>()
+    val toastText: LiveData<Event<String>> = _toastText
+
+    private val _isSucceed = MutableLiveData<Event<Boolean>>()
+    val isSucceed: LiveData<Event<Boolean>> = _isSucceed
+
+    fun postStory(s: Session, date: String, cerita: String) {
+        _isLoading.value = true
+        val name = s.name as String
+        val userId = s.id as String
+        postStoryBaseUrlAll(name, date, cerita)
+        postStoryBaseUrlUser(name, userId, date, cerita)
+    }
+
+    private fun postStoryBaseUrlAll(name: String, date: String, cerita: String) {
+        val baseUrl = ApiConfig.ALL_CERITA_URL
+        val client = ApiConfig.getApiService(baseUrl).postStory(name, date, cerita)
+        client.enqueue(object : Callback<Cerita> {
+            override fun onResponse(
+                call: Call<Cerita>,
+                response: Response<Cerita>
+            ) {
+//                _isLoading.value = false
+                if (response.isSuccessful) {
+                    // _isSucceed.value = Event(true)
+                } else {
+                    _toastText.value = Event(response.message())
+                    Log.e(TAG, "onFailure x: ${response.message()}")
+//                    response.errorBody()?.let {
+//                        val jObjError = JSONObject(it.string())
+//                        _toastText.value = Event(jObjError.getString("message"))
+//                    } ?: let {
+//                        _toastText.value = Event(response.message())
+//                    }
+                }
+            }
+            override fun onFailure(call: Call<Cerita>, t: Throwable) {
+//                _isLoading.value = false
+                _toastText.value = Event(t.message.toString())
+                Log.e(TAG, "onFailure y: ${t.message}")
+            }
+        })
+
+    }
+
+    private fun postStoryBaseUrlUser(name: String, userId: String, date: String, cerita: String) {
+        val baseUrl = ApiConfig.USER_CERITA_URL
+        val client = ApiConfig.getApiService(baseUrl).postStoryWithUserId(name, userId, date, cerita)
+        client.enqueue(object : Callback<Cerita> {
+            override fun onResponse(
+                call: Call<Cerita>,
+                response: Response<Cerita>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _isSucceed.value = Event(true)
+                } else {
+                    _toastText.value = Event(response.message())
+                    Log.e(TAG, "onFailure x: ${response.message()}")
+//                    response.errorBody()?.let {
+//                        val jObjError = JSONObject(it.string())
+//                        _toastText.value = Event(jObjError.getString("message"))
+//                    } ?: let {
+//                        _toastText.value = Event(response.message())
+//                    }
+                }
+            }
+            override fun onFailure(call: Call<Cerita>, t: Throwable) {
+                _isLoading.value = false
+                _toastText.value = Event(t.message.toString())
+                Log.e(TAG, "onFailure y: ${t.message}")
+            }
+        })
+    }
+
 }
